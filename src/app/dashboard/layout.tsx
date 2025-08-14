7ff01765a9434815166828b9d9df69e6d222bc59
sheikhsohag <sheikhsohag000@gmail.com>
2025-08-14 09:23:23 +0600

    file structure and dashboard added

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function layout() {
+  return (
+    <div>layout dashboard</div>
+  )
+}
+
+export default layout
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <li>about</li>
+    </div>
+  )
+}
+
+export default page
